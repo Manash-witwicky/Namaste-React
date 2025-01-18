426f3c5ae08b859665973a3b42dd32e3b1563d5e
Manash <manashdas29@gmail.com>
2025-01-19 00:56:32 +0530

    Food app Header completed

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-/**
- *  <div id="parent">
- *      <div id="child">
- *          <h1>I am an h1 tag</h1>
- *          <h2>I am an h2 tag</h2>
- *      </div>
- * </div>
- */
-
-// Above HTML code in plain React
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Logo from "./assets/logo.png";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "first" }, "I am an h1 tag"),
-    React.createElement("h2", { id: "second" }, "I am an h2 tag"),
-  ])
-);
-console.log(parent);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={Logo} className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);

96170f85f904f1760e8f3669998a2ba07df62555
Manash <manashdas29@gmail.com>
2025-02-10 01:21:41 +0530

    Filter restaurants and added dark bg

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import { RestaurantCard } from "./RestaurantCard";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { API_URL } from "../../utils/constants";
 import Shimmer from "./Shimmer";
 
@@ -8,6 +8,9 @@ const Body = () => {
   // const [listOfRestaurants, setListOfRestaurants] = arr;
   // console.log(arr); array destructuring
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [noResult, setNoResult] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -27,6 +30,9 @@ const Body = () => {
       setListOfRestaurants(
         restaurants[1].card.card.gridElements.infoWithStyle.restaurants
       );
+      setFilteredRestaurants(
+        restaurants[1].card.card.gridElements.infoWithStyle.restaurants
+      );
     } catch (error) {
       if (error instanceof TypeError) {
         console.log(`Network Error: ${error?.message}`);
@@ -34,24 +40,56 @@ const Body = () => {
     }
   };
 
-  const filteredRestaurants = () => {
-    const filteredRestaurants = listOfRestaurants.filter(
+  const topRatedRestaurants = () => {
+    const topRatedRestaurants = listOfRestaurants.filter(
       (res) => res?.info?.avgRating > 4.3
     );
-    setListOfRestaurants(filteredRestaurants);
+    setListOfRestaurants(topRatedRestaurants);
+  };
+
+  const filterRestaurants = () => {
+    // When searchText is an empty string,
+    // the includes method will always return true for any string,
+    // because every string contains an empty substring.
+    // so, filter method returns all the restaurants
+    const data = searchText
+      ? listOfRestaurants.filter((res) =>
+          res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+      : listOfRestaurants;
+    if (!searchText) setNoResult(false);
+    if (!data.length) setNoResult(true);
+    setFilteredRestaurants(data);
   };
 
   return !listOfRestaurants.length ? (
     <Shimmer />
   ) : (
     <div className="body-container">
-      <div className="search">
-        <button onClick={filteredRestaurants}>Top Rated Restaurants</button>
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search for Restaurants"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button onClick={filterRestaurants}>Search</button>
+        </div>
+        <button className="top-res" onClick={topRatedRestaurants}>
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
-        ))}
+        {noResult ? (
+          <p>No Result Found</p>
+        ) : (
+          filteredRestaurants.map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+          ))
+        )}
       </div>
     </div>
   );

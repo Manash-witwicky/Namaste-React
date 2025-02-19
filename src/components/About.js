326b43b197132de84be13c7deb2adfee4daa49bb
Manash <manashdas29@gmail.com>
2025-02-20 01:53:01 +0530

    worked on class component lifecycle

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,26 @@
-import User from "./User";
+import { Component } from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <User name={"Manash"} location={"Bangalore"} contact={"Functional"} />
-      <UserClass name={"Manash"} location={"Bangalore"} contact={"Class"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("About Constructor called");
+  }
+
+  componentDidMount() {
+    console.log("About componentDidMount called");
+  }
+
+  render() {
+    console.log("About Render Called");
+
+    return (
+      <div>
+        <h1>About Page</h1>
+        <UserClass name={"Manash"} location={"Bangalore"} contact={"Class"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,17 +1,63 @@
 import React from "react";
+import { Component } from "react";
 
-class UserClass extends React.Component {
+// class userClass extends React.Component
+class UserClass extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      count: 0,
+      userInfo: {
+        id: 100,
+      },
+    };
+    console.log("UserClass Constructor called");
   }
+
+  async componentDidMount() {
+    const res = await fetch("https://api.github.com/users/Manash-witwicky");
+    const data = await res.json();
+    this.setState({
+      userInfo: data,
+    });
+    console.log("User class componentDidMount called");
+  }
+
+  /**
+   * it accepts 2 args
+   * prevProps: The props that the component received before the update.
+   *prevState: The state that the component had before the update.
+   */
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    console.log("Component Destroyed");
+  }
+
+  increaseCount = () => {
+    // setState() again trigger render() method
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
   render() {
+    console.log("UserClass Render called");
     const { name, location, contact } = this.props;
+    const { count } = this.state;
+    const { id } = this.state.userInfo;
     return (
       <div className="user-card">
+        <h2>Count: {count}</h2>
+        <button onClick={this.increaseCount}>Increment</button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
+        <h1>GitHub ID</h1>
+        <h3>{id}</h3>
       </div>
     );
   }
